065b933468c7f50e99bf389a86570ffae45df91b
Michael Ruelle <mikeruelle@comcast.net>
2004-12-13 00:05:32 +0000

    allow for new tvtime not using the -D option anymore
    git-svn-id: svn://svn.freevo.org/freevo/branches/rel-1-5/freevo@6754 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- src/tv/plugins/tvtime.py
+++ src/tv/plugins/tvtime.py
@@ -9,6 +9,9 @@
 #
 # -----------------------------------------------------------------------
 # $Log$
+# Revision 1.38.2.1  2004/12/13 00:05:32  mikeruelle
+# allow for new tvtime not using the -D option anymore
+#
 # Revision 1.38  2004/07/10 12:33:42  dischi
 # header cleanup
 #
@@ -88,18 +91,24 @@ class PluginInterface(plugin.Plugin):
         self.tvtimecache = os.path.join(config.FREEVO_CACHEDIR, 'tvtimecache')
         self.mylocalconf_t = os.path.getmtime(self.mylocalconf)
         self.myfconfig_t = os.path.getmtime(self.myfconfig)
+        self.major = 0
+        self.minor = 0
+        self.minorversion = 0
 
+        self.getTvtimeVersion()
         self.xmltv_supported = self.isXmltvSupported()
+        self.optionD_supported = self.isOptionDSupported()
 
         #check/create the stationlist.xml and tvtime.xml
-	self.createTVTimeConfig()
+        self.createTVTimeConfig()
 
         # create the tvtime object and register it
         plugin.register(TVTime(), plugin.TV)
+        plugin.getbyname(plugin.TV).optionD_supported = self.optionD_supported
+        plugin.getbyname(plugin.TV).xmltv_supported = self.xmltv_supported
 
-    def isXmltvSupported(self):
+    def getTvtimeVersion(self):
         helpcmd = '%s --help' %  config.TVTIME_CMD
-        has_xmltv=False
         child = popen2.Popen3( helpcmd, 1, 100)
         data = child.childerr.readline() # Just need the first line
         if data:
@@ -108,15 +117,24 @@ class PluginInterface(plugin.Plugin):
                 _debug_("major is: %s" % data.group( "major" ))
                 _debug_("minor is: %s" % data.group( "minor" ))
                 _debug_("version is: %s" % data.group( "version" ))
-                major = int(data.group( "major" ))
-                minor = int(data.group( "minor" ))
-                ver = int(data.group( "version" ))
-                if major > 0:
-                    has_xmltv=True
-                elif major == 0 and minor == 9 and ver >= 10:
-                    has_xmltv=True
+                self.major = int(data.group( "major" ))
+                self.minor = int(data.group( "minor" ))
+                self.minorversion = int(data.group( "version" ))
         child.wait()
+
+    def isXmltvSupported(self):
+        has_xmltv=False
+        if self.major > 0:
+            has_xmltv=True
+        elif self.major == 0 and self.minor == 9 and self.minorversion >= 10:
+            has_xmltv=True
         return has_xmltv
+ 
+    def isOptionDSupported(self):
+        has_D=False
+        if self.major == 0 and self.minor == 9 and self.minorversion < 13:
+            has_D=True
+        return has_D
 
     def findLocalConf(self):
         cfgfilepath = [ '.', os.path.expanduser('~/.freevo'), '/etc/freevo' ]
@@ -165,45 +183,44 @@ class PluginInterface(plugin.Plugin):
         if (long(self.myfconfig_t) > long(cachefconf_t)):
             _debug_('fconfig modified')
             return 1
-
-	return 0
+        return 0
 
     def readMtimeCache(self):
-	return file(self.tvtimecache, 'rb').readlines()
+        return file(self.tvtimecache, 'rb').readlines()
 
     def writeMtimeCache(self):
-	fp = open(self.tvtimecache, 'wb')
+        fp = open(self.tvtimecache, 'wb')
         fp.write(self.mylocalconf)
-	fp.write('\n')
+        fp.write('\n')
         fp.write(str(self.mylocalconf_t))
-	fp.write('\n')
+        fp.write('\n')
         fp.write(str(self.myfconfig_t))
-	fp.write('\n')
-	fp.close()
+        fp.write('\n')
+        fp.close()
 
     def writeTvtimeXML(self):
         tvtimexml = os.path.join(os.environ['HOME'], '.tvtime', 'tvtime.xml')
-	configcmd = os.path.join(os.path.dirname(config.TVTIME_CMD), "tvtime-configure")
+        configcmd = os.path.join(os.path.dirname(config.TVTIME_CMD), "tvtime-configure")
         #cf_norm, cf_input, cf_clist, cf_device = config.TV_SETTINGS.split()
-	fc = FreevoChannels()
-	vg = fc.getVideoGroup(config.TV_CHANNELS[0][2])
-	cf_norm = vg.tuner_norm
-	cf_input = vg.input_num
-	cf_device = vg.vdev
+        fc = FreevoChannels()
+        vg = fc.getVideoGroup(config.TV_CHANNELS[0][2])
+        cf_norm = vg.tuner_norm
+        cf_input = vg.input_num
+        cf_device = vg.vdev
         s_norm = cf_norm.upper()
-	daoptions = ''
+        daoptions = ''
         if os.path.isfile(tvtimexml):
-	    daoptions = ' -F ' + tvtimexml
+            daoptions = ' -F ' + tvtimexml
         if self.xmltv_supported:
             daoptions += ' -d %s -n %s -t %s' % (cf_device, s_norm, config.XMLTV_FILE)
         else:
             daoptions += ' -d %s -n %s' % (cf_device, s_norm)
-	if hasattr(config, "TV_TVTIME_SETUP_OPTS") and config.TV_TVTIME_SETUP_OPTS:
+        if hasattr(config, "TV_TVTIME_SETUP_OPTS") and config.TV_TVTIME_SETUP_OPTS:
             daoptions += ' %s' % config.TV_TVTIME_SETUP_OPTS
         os.system(configcmd+daoptions)
 
     def writeStationListXML(self):
-	self.createChannelsLookupTables()
+        self.createChannelsLookupTables()
         norm='freevo'
         tvnorm = config.CONF.tv
         tvnorm = tvnorm.upper()
@@ -241,14 +258,14 @@ class PluginInterface(plugin.Plugin):
         c = 0
         for m in config.TV_CHANNELS:
             mychan = m[2]
-	    myband = self.lookupChannelBand(mychan)
+            myband = self.lookupChannelBand(mychan)
             if myband == "Custom":
                 mychan = config.FREQUENCY_TABLE.get(mychan)
                 mychan = float(mychan)
                 mychan = mychan / 1000.0
                 mychan = "%.2fMHz" % mychan
             if (hasattr(config, 'TV_PAD_CHAN_NUMBERS') and config.TV_PAD_CHAN_NUMBERS and re.search('^\d+$', mychan)):
-	        for i in range(c,int(mychan)):
+                for i in range(c,int(mychan)):
                     fchild =  doc.createElement('station')
                     fchild.setAttribute('channel',str(i))
                     fchild.setAttribute('band',myband)
@@ -267,21 +284,21 @@ class PluginInterface(plugin.Plugin):
                 fchild.setAttribute('xmltvid',m[0])
             freevonode.appendChild(fchild)
             c = c + 1
-	# YUCK:
+        # YUCK:
         # PrettyPrint the results to stationlistxml unfortuneately it
-	# adds a bunch of stuff in comments at the end of the file
-	# that causes the document not to load if we merge again later
-	# so I print to a string buffer and then remove the offending
-	# comments by truncating the output.
-	strIO = StringIO()
+        # adds a bunch of stuff in comments at the end of the file
+        # that causes the document not to load if we merge again later
+        # so I print to a string buffer and then remove the offending
+        # comments by truncating the output.
+        strIO = StringIO()
         PrettyPrint(doc, strIO)
-	mystr = strIO.getvalue()
-	myindex = mystr.find('</stationlist>')
-	mystr = mystr[:myindex+15]
-	# how can 4suite be so stupid and still survive?
-	mystr = mystr.replace('<!DOCTYPE stationlist PUBLIC "http://tvtime.sourceforge.net/DTD/stationlist1.dtd" "-//tvtime//DTD stationlist 1.0//EN">','<!DOCTYPE stationlist PUBLIC "-//tvtime//DTD stationlist 1.0//EN" "http://tvtime.sourceforge.net/DTD/stationlist1.dtd">')
+        mystr = strIO.getvalue()
+        myindex = mystr.find('</stationlist>')
+        mystr = mystr[:myindex+15]
+        # how can 4suite be so stupid and still survive?
+        mystr = mystr.replace('<!DOCTYPE stationlist PUBLIC "http://tvtime.sourceforge.net/DTD/stationlist1.dtd" "-//tvtime//DTD stationlist 1.0//EN">','<!DOCTYPE stationlist PUBLIC "-//tvtime//DTD stationlist 1.0//EN" "http://tvtime.sourceforge.net/DTD/stationlist1.dtd">')
         fp = open(tvtimefile,'wb')
-	fp.write(mystr)
+        fp.write(mystr)
         fp.close()
 
 
@@ -296,14 +313,14 @@ class PluginInterface(plugin.Plugin):
         c = 0
         for m in config.TV_CHANNELS:
             mychan = str(m[2])
-	    myband = self.lookupChannelBand(mychan)
+            myband = self.lookupChannelBand(mychan)
             if myband == "Custom":
                 mychan = config.FREQUENCY_TABLE.get(mychan)
                 mychan = float(mychan)
                 mychan = mychan / 1000.0
                 mychan = "%.2fMHz" % mychan
             if (hasattr(config, 'TV_PAD_CHAN_NUMBERS') and config.TV_PAD_CHAN_NUMBERS and re.search('^\d+$', mychan)):
-	        for i in range(c,int(mychan)):
+                for i in range(c,int(mychan)):
                     fp.write('    <station name="%s" active="0" position="%s" band="%s" channel="%s"/>\n' % (i,i,myband,i))
                     c = c + 1
             if self.xmltv_supported:
@@ -319,19 +336,19 @@ class PluginInterface(plugin.Plugin):
     def lookupChannelBand(self, channel):
         # check if we have custom
        
-	#Aubin's auto detection code works only for numeric channels and
-	#forces them to int.
-	channel = str(channel)
+        #Aubin's auto detection code works only for numeric channels and
+        #forces them to int.
+        channel = str(channel)
        
         if config.FREQUENCY_TABLE.has_key(channel):
             _debug_("have a custom")
             return "Custom"
         elif (re.search('^\d+$', channel)):
             _debug_("have number")
-	    if self.chanlists.has_key(config.CONF.chanlist):
-                _debug_("found chanlist in our list")
-	        return self.chanlists[config.CONF.chanlist]
-	elif self.chans2band.has_key(channel):
+        if self.chanlists.has_key(config.CONF.chanlist):
+            _debug_("found chanlist in our list")
+            return self.chanlists[config.CONF.chanlist]
+        elif self.chans2band.has_key(channel):
             _debug_("We know this channels band.")
             return self.chans2band[channel]
         _debug_("defaulting to USCABLE")
@@ -361,7 +378,7 @@ class PluginInterface(plugin.Plugin):
         chans_list += [('U' + str(x), 'UHF') for x in range(21, 70)]
         chans_list += [('AU' + str(x), 'UHF Australia') for x in range(28, 70)]
         chans_list += [('O' + str(x), 'Australia Optus') for x in range(1, 58)]
-	self.chans2band=dict(chans_list)
+        self.chans2band=dict(chans_list)
 
 
 class TVTime:
@@ -371,8 +388,10 @@ class TVTime:
     
     def __init__(self):
         self.app_mode = 'tv'
-	self.fc = FreevoChannels()
+        self.fc = FreevoChannels()
         self.current_vg = None
+        self.optionD_supported = False
+        self.xmltv_supported = False
 
     def TunerSetChannel(self, tuner_channel):
         for pos in range(len(config.TV_CHANNELS)):
@@ -401,42 +420,46 @@ class TVTime:
         if mode == 'tv' or mode == 'vcr':
             
             w, h = config.TV_VIEW_SIZE
-	    cf_norm = vg.tuner_norm
-	    cf_input = vg.input_num
-	    cf_device = vg.vdev
+            cf_norm = vg.tuner_norm
+            cf_input = vg.input_num
+            cf_device = vg.vdev
 
             s_norm = cf_norm.upper()
 
-            outputplugin = config.CONF.display
-            if config.CONF.display == 'x11':
-                outputplugin = 'Xv'
-            elif config.CONF.display == 'mga':
-                outputplugin = 'mga'
-            elif config.CONF.display in ( 'directfb', 'dfbmga' ):
-                outputplugin = 'directfb'
+            outputplugin = ''
+            if plugin.getbyname(plugin.TV).optionD_supported:
+                if config.CONF.display == 'x11':
+                    outputplugin = 'Xv'
+                elif config.CONF.display == 'mga':
+                    outputplugin = 'mga'
+                elif config.CONF.display in ( 'directfb', 'dfbmga' ):
+                    outputplugin = 'directfb'
+                else:
+                    outputplugin = config.CONF.display
+                outputplugin = '-D %s' % outputplugin
 
             if mode == 'vcr':
-	        cf_input = '1'
-		if hasattr(config, "TV_VCR_INPUT_NUM") and config.TV_VCR_INPUT_NUM:
-		    cf_input = config.TV_VCR_INPUT_NUM
+                cf_input = '1'
+            if hasattr(config, "TV_VCR_INPUT_NUM") and config.TV_VCR_INPUT_NUM:
+                cf_input = config.TV_VCR_INPUT_NUM
 
             self.fc.chan_index = self.TunerSetChannel(tuner_channel)
 
             if hasattr(config, 'TV_PAD_CHAN_NUMBERS') and config.TV_PAD_CHAN_NUMBERS:
-	        mychan = tuner_channel
+                mychan = tuner_channel
             else:
-	        mychan = self.fc.chan_index
+                mychan = self.fc.chan_index
 
             _debug_('starting channel is %s' % mychan)
 
-            command = '%s -D %s -k -I %s -n %s -d %s -f %s -c %s -i %s' % (config.TVTIME_CMD,
+            command = '%s %s -k -I %s -n %s -d %s -f %s -c %s -i %s' % (config.TVTIME_CMD,
                                                                    outputplugin,
                                                                    w,
                                                                    s_norm,
                                                                    cf_device,
                                                                    'freevo',
                                                                    mychan,
-								   cf_input)
+                                   cf_input)
 
             if osd.get_fullscreen() == 1:
                 command += ' -m'
@@ -523,8 +546,8 @@ class TVTime:
             #    self.Play('tv', nextchan)
             #    return TRUE
 
-	    self.fc.chanSet(nextchan, app=self.app)
-	    #self.current_vg = self.fc.getVideoGroup(self.fc.getChannel())
+            self.fc.chanSet(nextchan, app=self.app)
+            #self.current_vg = self.fc.getVideoGroup(self.fc.getChannel())
 
             # Go to the prev/next channel in the list
             if event == em.TV_CHANNEL_UP:
@@ -539,20 +562,20 @@ class TVTime:
             return True
         
         elif event == em.OSD_MESSAGE:
-	    # XXX this doesn't work
+            # XXX this doesn't work
             #self.app.write('display_message %s\n' % event.arg)
-	    #this does
+            #this does
             os.system('tvtime-command display_message \'%s\'' % event.arg)
             return True
        
         elif event in em.INPUT_ALL_NUMBERS:
             self.app.write('CHANNEL_%s\n' % event.arg)
-	    
+        
         elif event == em.BUTTON:
             if event.arg == 'PREV_CH':
                 self.app.write('CHANNEL_PREV\n')
                 return True
-	        
+            
 
         return False
         
