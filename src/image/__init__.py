a6063694491604e4f29145a6dc116aa17ce08b04
Duncan Webb <duncan@freevo.org>
2006-10-14 08:05:26 +0000

    Removed old cvs log
    git-svn-id: svn://svn.freevo.org/freevo/branches/rel-1-5/freevo@8363 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- src/image/__init__.py
+++ src/image/__init__.py
@@ -8,14 +8,6 @@
 # Todo:        
 #
 # -----------------------------------------------------------------------
-# $Log$
-# Revision 1.20  2004/07/10 12:33:39  dischi
-# header cleanup
-#
-# Revision 1.19  2004/03/14 17:32:39  dischi
-# unicode fix
-#
-# -----------------------------------------------------------------------
 # Freevo - A Home Theater PC framework
 # Copyright (C) 2002 Krister Lagerstrom, et al. 
 # Please see the file freevo/Docs/CREDITS for a complete list of authors.
@@ -34,7 +26,7 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 #
-# ----------------------------------------------------------------------- */
+# -----------------------------------------------------------------------
 
 
 import os
