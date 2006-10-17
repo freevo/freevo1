89f0e4beda2d69ef4794e0b8f3a8cd580a9bf701
Duncan Webb <duncan@freevo.org>
2006-10-17 15:58:31 +0000

    [ 1578402 ] Include the DVD encoding package Incorporated into freevo.
    git-svn-id: svn://svn.freevo.org/freevo/branches/rel-1/freevo@8400 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

---
+++ src/helpers/encodingserver.py
@@ -0,0 +1,208 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+# -----------------------------------------------------------------------
+# EncodingServer.py, part of EncodingServer - for use with or without Freevo
+# -----------------------------------------------------------------------
+# $Id: rc.py 8278 2006-09-30 07:22:11Z duncan $
+#
+# Author: den_RDC
+# some parts taken or inspired by Freevo's recordserver (by rshortt)
+# TODO:
+# niceness & pausing queue
+#
+# -----------------------------------------------------------------------
+# Copyright (C) 2004 den_RDC (RVDM)
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MER-
+# CHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation
+#
+# -----------------------------------------------------------------------
+
+#Import statements
+from twisted.web import xmlrpc, server
+from twisted.internet.app import Application
+from twisted.internet import reactor
+from twisted.persisted import marmalade
+import logging
+
+import time, random, sys, os
+
+from encodingcore import EncodingJob, EncodingQueue
+
+#some data
+__author__ = "den_RDC (rdc@kokosnoot.com)"
+__revision__ = "$Rev: 26 $"
+__copyright__ = "Copyright (C) 2004 den_RDC"
+__license__ = "GPL"
+
+tmppath = '/tmp/encodingserver'
+
+DEBUG=False
+
+jam = marmalade.jellyToXML
+unjam = marmalade.unjellyFromXML
+
+class EncodingServer(xmlrpc.XMLRPC):
+
+    def __init__(self, debug=False):
+        self.jobs = {}
+        
+        #setup a logger
+        if debug: #when debugging, output everything to stdout using the root logging class
+            self.log = logging
+        else: #normally, write nice formatted messages to a logfile)
+            self.log = logging.getLogger("EncodingServer")
+            self.log.setLevel(logging.INFO)
+            FHandler = logging.FileHandler("encodingserver.log")
+            FHandler.setFormatter(logging.Formatter("%(asctime)s:%(levelname)s %(message)s"))
+            self.log.addHandler(FHandler)
+        
+        self.queue = EncodingQueue(self.log)
+        
+        self.log.info("EncodingServer started...")
+        
+    def xmlrpc_echotest(self, blah):
+        return (True, 'EncodingServer::echotest: %s' % blah)
+
+    def xmlrpc_initEncodeJob(self, source, output, friendlyname="", chapter=None):
+        #safety checks
+        if not (source or output):
+            return (False, 'EncodingServer::initEncodeJob:  no source or output given')
+        #generate a "random" idnr based on the time
+        #in p2.3 , int() can return long int's, wich is fine, except it makes XMLRPC fail somwhere along the way
+        # so we devide or random number by 100 :)
+        idnr = int((time.time() / random.random()) / 100)
+        self.jobs[idnr] = EncodingJob(source, output, friendlyname, idnr, chapter)
+        
+        #wait for the analyzing to end
+        while not self.jobs[idnr].finishedanalyze:
+            time.sleep(0.1)
+        
+        self.log.info("Initialized job %s (idnr : %s)" % (friendlyname,idnr))
+        
+        return (True, idnr)
+        
+    def xmlrpc_getContainerCAP(self, idnr):
+        return (True, jam(self.jobs[idnr].getContainerList()))
+       
+    def xmlrpc_setContainer(self, idnr, container):
+        status = self.jobs[idnr].setContainer(container)
+        
+        if not status:
+            return (True, "EncodingServer::setContainer: OK")
+        else:
+            return (False, "EncodingServer::setContainer: %s" % status)
+            
+    def xmlrpc_getVideoCodecCAP(self, idnr):
+        return (True, jam(self.jobs[idnr].getVideoCodecList()))
+        
+    def xmlrpc_setVideoCodec(self, idnr, vcodec, tgtsize, multipass=False):
+        #safety checks
+        if not (vcodec or tgtsize):
+            return (False, 'EncodingServer::setVideoCodec:  no codec or target size given')
+            
+        status = self.jobs[idnr].setVideoCodec(vcodec, tgtsize, multipass)
+        
+        if not status:
+            return (True, "EncodingServer::setVideoCodec: OK")
+        else:
+            return (False, "EncodingServer::setVideoCodec: %s" % status)   
+           
+    def xmlrpc_getAudioCodecCAP(self, idnr):
+        return (True, jam(self.jobs[idnr].getAudioCodecList()))
+        
+    def xmlrpc_setAudioCodec(self, idnr, acodec, abrate):
+        #safety checks
+        if not (acodec or abrate):
+            return (False, 'EncodingServer::setAudioCodec:  no codec or bitrate given')
+            
+        status = self.jobs[idnr].setAudioCodec(acodec, abrate)
+        
+        if not status:
+            return (True, "EncodingServer::setAudioCodec: OK")
+        else:
+            return (False, "EncodingServer::setAudioCodec: %s" % status)
+            
+    def xmlrpc_getVideoFiltersCAP(self, idnr):
+        return (True, jam(self.jobs[idnr].getVideoFiltersList()))
+        
+
+    def xmlrpc_setVideoFilters(self, idnr, filters):
+        #safety checks
+        if not filters:
+            return (False, 'EncodingServer::setAudioCodec:  no codec or bitrate given')
+            
+        status = self.jobs[idnr].setVideoFilters(unjam(filters))
+        
+        if not status:
+            return (True, "EncodingServer::setVideoFilters: OK")
+        else:
+            return (False, "EncodingServer::setVideoFilters: %s" % status)
+        
+    def xmlrpc_queueIt(self, idnr, now=False):
+        self.queue.addEncodingJob(self.jobs[idnr])
+        del self.jobs[idnr]
+        self.log.info("Added job %s to the queue" % idnr)
+        if now:
+            self.queue.startQueue()
+        return (True, "EncodingServer::queueIt: OK")
+        
+    def xmlrpc_getProgress(self):
+        prog = self.queue.getProgress()
+        if type(prog) is str:
+            return (False, "EncodingServer::getProgress: %s" % prog)
+        return (True, jam(prog))
+        
+    def xmlrpc_startQueue(self):
+        self.queue.startQueue()
+        
+        return (True, "EncodingServer::startqueue: OK")
+        
+    def xmlrpc_listJobs(self):
+        jlist = self.queue.listJobs()
+        
+        return (True, jam(jlist))
+
+        
+        
+
+def main():
+    #check for /tmp/encodingserver and if it doesn't exist make it
+    if not (os.path.exists(tmppath) and os.path.isdir(tmppath)):
+        os.mkdir(tmppath)
+    #chdir to /tmp/encodingserver
+    os.chdir(tmppath)
+    
+    app = Application("EncodingServer")
+    if len(sys.argv) >= 2 and sys.argv[1] == "debug":
+        es = EncodingServer(True)
+        DEBUG=True
+        import encodingcore
+        encodingcore.DEBUG=True
+    else:
+        es = EncodingServer()
+    app.listenTCP(6666, server.Site(es))
+    app.run(save=0)
+    
+
+if __name__ == '__main__':
+    import traceback
+    while 1:
+        try:
+            start = time.time()
+            main()
+            break
+        except:
+            traceback.print_exc()
+            if start + 10 > time.time():
+                print 'server problem, sleeping 1 min'
+                time.sleep(60)
---
+++ src/video/plugins/dvdbackup.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+# -----------------------------------------------------------------------
+# dvdbackup.py - Plugin for encoding DVD's with the EncodingServer
+# -----------------------------------------------------------------------
+# $Id: rc.py 8278 2006-09-30 07:22:11Z duncan $
+#
+# Author: 
+# TODO:
+# niceness & pausing queue
+#
+# -----------------------------------------------------------------------
+# Copyright (C) 2004 den_RDC (RVDM)
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MER-
+# CHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation
+#
+# -----------------------------------------------------------------------
+
+#Import statements
+from os.path import join, split
+import plugin, config, menu
+#import config
+from encodingclient import *
+from gui.AlertBox import AlertBox
+from gui.PopupBox import PopupBox
+
+class PluginInterface(plugin.ItemPlugin):
+    """
+    PLugin for making DVD backups (aka dvdrips) using EncodingServer
+    
+    This plugin NEEDS a running encodingserver to work properly.
+    You can start an encodingserver with "freevo encodingserver start".
+    Don't forget that you need some free diskspace in order to use this plugin, 
+    and don't forget a dvdrip eats quite a bit of space :)
+    """
+
+    def actions(self, item):
+        #testing stuff
+##~         print item.mode
+##~         print item.name
+##~         print item.filename
+##~         print item.parentname
+##~         if hasattr(item, 'media') and hasattr(item.media, 'devicename'):
+##~             print item.media.devicename
+            
+        if item.type == 'video' and item.mode == 'dvd' and hasattr(item, 'info_type'):
+            if item.info_type == "track": #and item.media and item.media.devicename: 
+                #for dvd on disc
+                self.dvdsource = item.filename
+                self.title = int(split(item.url)[-1])
+                
+                if hasattr(item, 'media') and hasattr(item.media, 'devicename'):
+                    #we have a "real" dvd disc
+                    self.dvdsource = item.media.devicename
+                    self.title = int(item.url[6:])
+
+                self.item = item
+                return [ (self.encoding_profile_menu, _('Backup this dvd title...')) ]
+        return []
+        
+    def encoding_profile_menu(self, menuw=None, arg=None):
+        #create a menu with a few encoding options (1cd, 2cd, xvid, mpeg4)
+        #args : tuple, (videocodec, size, multipass
+        menu_items = [ menu.MenuItem("XViD, 700mb", self.create_job, (1,700,False)) ]
+        menu_items.append( menu.MenuItem("XViD, 700mb, High Quality", self.create_job, (1,700,True)) )
+        menu_items.append( menu.MenuItem("XViD, 1400mb", self.create_job, (1,1400,False)) )
+        menu_items.append( menu.MenuItem("XViD, 1400mb, High Quality", self.create_job, (1,1400,True)) )
+        menu_items.append( menu.MenuItem("DivX, 700mb", self.create_job, (0,700,False)) )
+        menu_items.append( menu.MenuItem("DivX, 700mb, High Quality", self.create_job, (0,700,True)) )
+        menu_items.append( menu.MenuItem("DivX, 1400mb", self.create_job, (0,1400,False)) )
+        menu_items.append( menu.MenuItem("DivX, 1400mb, High Quality", self.create_job, (0,1400,True)) )
+        
+        encoding_menu = menu.Menu(_('Choose your encoding profile'), menu_items)
+        menuw.pushmenu(encoding_menu)
+        
+    def create_job(self, menuw=None, arg=None):
+        #create a filename for the to-be-encoded dvd title
+        #title = int(self.item.url[6:])
+        fname = join(config.VIDEO_ITEMS[0][1], "%s_%s.avi" % (self.item.parent.name, self.title))
+        #print title, fname
+        print arg
+        #unwrap settings tupple
+        vcodecnr, tgtsize, mpass = arg
+
+        #we are going to create a job and send it to the encoding server, this can take some time while analyzing
+        
+        box = PopupBox(text=_('Please wait, analyzing video...'))
+        box.show()
+
+        (status, resp) = initEncodeJob(self.dvdsource, fname, 
+                self.item.parent.name, self.title)
+                
+        box.destroy()
+        
+        if not status:
+            self.error(resp)
+            return
+            
+        idnr = resp
+        
+        #ask for possible containers and set the first one (should be avi), we will get a list
+        (status, resp) = getContainerCAP(idnr)
+            
+        if not status:
+            self.error(resp)
+            return
+        
+        container = resp[0]
+        
+        (status, resp) = setContainer(idnr, container)
+        
+        if not status:
+            self.error(resp)
+            return
+            
+        #ask for possible videocodec and set the first one (should be mpeg4), we will get a list
+        (status, resp) = getVideoCodecCAP(idnr)
+            
+        if not status:
+            self.error(resp)
+            return
+        
+        vcodec = resp[vcodecnr]
+        
+        (status, resp) = setVideoCodec(idnr, vcodec, tgtsize, mpass)
+        
+        if not status:
+            self.error(resp)
+            return
+            
+        #ask for possible audiocodec and set the first one (should be mp3), we will get a list
+        #Audiocodec call isn't necessary atm, it defaults to 128 kbit mp3, but this might change in the future
+        #so we play safe
+        (status, resp) = getAudioCodecCAP(idnr)
+            
+        if not status:
+            self.error(resp)
+            return
+        
+        acodec = resp[0]
+        
+        (status, resp) = setAudioCodec(idnr, acodec, 128)
+        
+        if not status:
+            self.error(resp)
+            return
+        
+        #And finally, qeue and start the job
+        (status, resp) = queueIt(idnr, True)
+            
+        if not status:
+            self.error(resp)
+            return
+        
+        self.menuw = menuw
+        AlertBox(width=400, height=200, text=_("Encoding started"), handler=self.mopup).show()
+        
+        print "boe"
+        #menuw.delete_menu()
+        #menuw.delete_menu()       
+        
+        
+        
+    def error(self, text=""):
+        AlertBox(width=400, height=200, text="ERROR: %s" % text).show()
+        
+    
+    def mopup(self):
+        self.menuw.delete_menu()
+        self.menuw.back_one_menu()
