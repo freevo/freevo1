afc6c9e47a0168045b7ffa0e3fb6347da2fb7b62
Duncan Webb <duncan@freevo.org>
2006-11-16 21:10:06 +0000

    Updated diskfree to show a percentage used bar instead of a pie chart
    git-svn-id: svn://svn.freevo.org/freevo/branches/rel-1/freevo@8580 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- src/plugins/idlebar/diskfree.py
+++ src/plugins/idlebar/diskfree.py
@@ -29,7 +29,7 @@
 
 # python modules
 import time
-import os
+import os, pygame
 
 # freevo modules
 from plugins.idlebar import IdleBarPlugin
@@ -51,7 +51,28 @@ class PluginInterface(IdleBarPlugin):
         self.plugin_name = 'idlebar.diskfree'
         self.time = 0
         self.diskfree = 0
+        self.freespace = 0
+        self.totalspace = 0
+        self.percent = 0.0
 
+        self.diskimg = os.path.join(config.ICON_DIR, 'status/diskfree.png')
+        self.goodimg = os.path.join(config.ICON_DIR, 'status/diskfree-good.png')
+        self.poorimg = os.path.join(config.ICON_DIR, 'status/diskfree-poor.png')
+        self.badimg  = os.path.join(config.ICON_DIR, 'status/diskfree-bad.png')
+        self.cacheimg = {}
+
+
+    def getimage(self, image, osd, cache=False):
+        if image.find(config.ICON_DIR) == 0 and image.find(osd.settings.icon_dir) == -1:
+            new_image = os.path.join(osd.settings.icon_dir, image[len(config.ICON_DIR)+1:])
+            if os.path.isfile(new_image):
+                image = new_image
+        if cache:
+            if image not in self.cacheimg.keys():
+                self.cacheimg[image] = pygame.image.load(image)
+            return self.cacheimg[image]
+
+        return pygame.image.load(image)
 
     def getDiskFree(self):
         """
@@ -59,22 +80,35 @@ class PluginInterface(IdleBarPlugin):
         Update maximum every 30 seconds
 
         """
-        if (time.time()-self.time)>30:
+        if (time.time()-self.time) > 30:
             self.time = time.time()
             freespace = util.freespace(config.TV_RECORD_DIR)
-            self.diskfree = _('%iGb') % (((freespace / 1024) / 1024) / 1024)
+            totalspace = util.totalspace(config.TV_RECORD_DIR)
+            self.diskfree = _('%iGB') % (((freespace / 1024) / 1024) / 1024)
+            self.freespace = (((freespace / 1024) / 1024) / 1024)
+            self.totalspace = (((totalspace / 1024) / 1024) / 1024)
+            self.percent = (self.totalspace - self.freespace) * 1.0 / self.totalspace
+
 
     def draw(self, (type,object),x,osd):
         """
         Drawing to idlebar
         """
-
         self.getDiskFree()
+        diskimg = self.getimage(self.diskimg, osd)
+        w,h = diskimg.get_size()
+        if self.freespace < 8:
+            diskbar = self.getimage(self.badimg, osd, True)
+        elif self.freespace < 20:
+            diskbar = self.getimage(self.poorimg, osd, True)
+        else:
+            diskbar = self.getimage(self.goodimg, osd, True)
+        diskimg.blit(diskbar, (0,0), (0, 0, (w * self.percent), h))
+        self.cacheimg['cached'] = diskimg
         font = osd.get_font('small0')
         widthdf = 0
         widthdf = font.stringsize(self.diskfree)
-        osd.draw_image(os.path.join(config.ICON_DIR, 'misc/chartpie.png' ),(x, osd.y + 7, -1, -1))
+        w = osd.drawimage(diskimg, (x, osd.y + 7, -1, -1) )[0]
         osd.write_text(self.diskfree, font, None, x + 15, osd.y + 55 - font.h, widthdf, font.h, 'left', 'top')
 
         return widthdf + 15
-
