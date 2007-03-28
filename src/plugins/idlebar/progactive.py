f1f24af3fa7fb66d5860ed076726d735c4e2b859
Duncan Webb <duncan@freevo.org>
2007-03-28 18:56:38 +0000

    Corrected progactive plug-in to use the status directory Added progactive to Changelog
    git-svn-id: svn://svn.freevo.org/freevo/branches/rel-1/freevo@9408 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- src/plugins/idlebar/progactive.py
+++ src/plugins/idlebar/progactive.py
@@ -21,11 +21,6 @@
 # This program is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of MER-
 # CHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
-"""
-
-done by despeyer >> www.despeyer.de >> mail@despeyer.de
-if you got questions, ideas, etc., please mail me !!!
-"""
 
 import time
 import os
@@ -37,50 +32,53 @@ import skin
 from pygame import image, transform
 from plugins.idlebar import IdleBarPlugin
 
-__version__="1.0"
-__author__="mail@despeyer.de"
-__doc__="""Display an Icon if prog (defined in local_conf.py) is active or not.
+__version__='1.0'
+__author__='mail@despeyer.de'
+__doc__='''Display an Icon if prog (defined in local_conf.py) is active or not.
 The Prog-Name is also drawn over the icon.
-"""
+'''
 
 class PluginInterface(IdleBarPlugin):
-    """
+    '''
     Display an Icon if prog is active, put following Lines in local_conf.py:
     plugin.activate('idlebar.progactive')
-    """
+    '''
     def __init__(self):
         IdleBarPlugin.__init__(self)
         self.plugin_name='idlebar.progactive'
         self.time = 0
-        self.progname = config.PROG_NAME
+        self.progname = config.PROGACTIVE_PROGRAM
+        icondir = os.path.join(config.ICON_DIR, 'status')
+        self.images = {}
+        self.images['cross'] = os.path.join(icondir, 'cross.png')
+        self.images['check'] = os.path.join(icondir, 'check.png')
+        self.image = self.images['cross']
         
     def config(self):
         '''config is called automatically, for default settings run:
         freevo plugins -i idlebar.progactive'''
-        return [ ('PROG_NAME', None, 'Program that you want to monitor') ]
+        return [ ('PROGACTIVE_PROGRAM', None, 'Program that you want to monitor') ]
 
     def getstatus(self):
-        """
+        '''
         Update every 30 secs
-        """
+        '''
         if (time.time()-self.time) > 30:
             self.time = time.time()
-            cmd = "ps -A | grep " + self.progname
+            cmd = 'ps -A | grep ' + self.progname
             fin, fout = os.popen4(cmd)
             result = fout.read()
-            if result == "":
-                self.image = 'cross.png'
+            if result == '':
+                self.image = self.images['cross']
             else:
-                self.image = 'check.png'
+                self.image = self.images['check']
         
     def draw(self, (type, object), x, osd):
         self.getstatus()
         font = osd.get_font('small0')
-        widthmt = 0
-        widthmt = font.stringsize(config.PROG_NAME)
-        image = self.image
-        osd.write_text(config.PROG_NAME, font, None, x + 15, osd.y + 55 - font.h, widthmt, font.h, 'left', 'top')
+        widthmt = font.stringsize(config.PROGACTIVE_PROGRAM)
+        osd.write_text(config.PROGACTIVE_PROGRAM, font, None, x + 15, osd.y + 55 - font.h, \
+            widthmt, font.h, 'left', 'top')
+        if not self.image:
+            return widthmt + 15
         return osd.drawimage(self.image, (x, osd.y + 10, -1, 45))[0]
-        return widthmt + 15
-
-
