a8c087e1fa15f5fee858167ef8d5f8d1ce5b55f4
Duncan Webb <duncan@freevo.org>
2008-02-21 20:49:20 +0000

    Consolidating differencies between rel-1 and rel-1-7
    git-svn-id: svn://svn.freevo.org/freevo/branches/rel-1/freevo@10410 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- src/www/configlib.py
+++ src/www/configlib.py
@@ -1,7 +1,7 @@
 # -*- coding: iso-8859-1 -*-
 # vim:autoindent:tabstop=4:softtabstop=4:shiftwidth=4:expandtab:filetype=python:
 # -----------------------------------------------------------------------
-# pluginconfig - update plugin settings in local_conf.py
+# Update plugin settings in local_conf.py
 # -----------------------------------------------------------------------
 # $Id$
 #
