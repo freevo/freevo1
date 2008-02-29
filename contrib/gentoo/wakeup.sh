38c2fc47ea30926d0d3965006af9ba525c45db1b
Duncan Webb <duncan@freevo.org>
2008-02-29 18:29:16 +0000

    Replaced back quotes with $(), this is easier to see and allows nested command Changed to allow an error message when the acpi devices are not found.
    git-svn-id: svn://svn.freevo.org/freevo/branches/rel-1/freevo@10456 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- contrib/gentoo/wakeup.sh
+++ contrib/gentoo/wakeup.sh
@@ -4,14 +4,14 @@ source /etc/conf.d/clock
 
 if [[ ${CLOCK} == "UTC" ]] ; then
     hwopts="--utc"
-    offset=`date +%z`
+    offset=$(date +%z)
 else
     hwopts="--localtime"
     offset="+0000"
 fi
 
-wakedate=`date -u -d "$1 ${offset}" "+%F %H:%M:%S"`
-wakesecs=`date -d "$1 ${offset}" +%s`
+wakedate=$(date -u -d "$1 ${offset}" "+%F %H:%M:%S")
+wakesecs=$(date -d "$1 ${offset}" +%s)
 
 #uncomment this line if you MB resets alarms
 #when hardware clock is updated on shutdown
@@ -21,7 +21,8 @@ wakesecs=`date -d "$1 ${offset}" +%s`
 if [ -e /sys/class/rtc/rtc0/wakealarm ]; then
     echo 0 > /sys/class/rtc/rtc0/wakealarm
     echo ${wakesecs} > /sys/class/rtc/rtc0/wakealarm
-fi
-if [ -e /proc/acpi/alarm ]; then
+elif [ -e /proc/acpi/alarm ]; then
     echo ${wakedate} > /proc/acpi/alarm
+else
+    echo "Cannot find acpi rtc" >&2
 fi
