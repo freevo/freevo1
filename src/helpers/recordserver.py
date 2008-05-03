3bbd1a1bae8e45875e92732527564baff4b40381
Duncan Webb <duncan@freevo.org>
2008-05-03 11:26:58 +0000

    [ 1955745 ] findMatches don't work with chars '().' in title Patch from Valera Koval applied
    git-svn-id: svn://svn.freevo.org/freevo/branches/rel-1/freevo@10666 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- src/helpers/recordserver.py
+++ src/helpers/recordserver.py
@@ -817,7 +817,7 @@ class RecordServer:
         max_results = 500
 
         if find:
-            find = Unicode(find)
+            find = Unicode(find).replace('(','\(').replace(')','\)').replace('.','\.')
         elif not movies_only:
             _debug_('nothing to find', DINFO)
             return (False, _('nothing to find'))
