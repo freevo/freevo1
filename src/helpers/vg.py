366ec3bf576f79b034e0bfbbd002e0a21c6fc7a5
Duncan Webb <duncan@freevo.org>
2008-05-10 20:39:13 +0000

    Added options and tidied up
    git-svn-id: svn://svn.freevo.org/freevo/branches/rel-1/freevo@10715 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- src/helpers/vg.py
+++ src/helpers/vg.py
@@ -1,7 +1,45 @@
+# -*- coding: iso-8859-1 -*-
+# -----------------------------------------------------------------------
+# Find video groups from Analog TV video devices
+# -----------------------------------------------------------------------
+# $Id$
 #
-# TV analyser
+# Notes:
+#   This module can be run under both python and freevo, only freevo mode
+#   will generate the video groups
+#   to run under python type:
+#       python /path/to/vg.py --help (normally src/helpers/vg.py)
+#   to run under freevo type:
+#       freevo vg --help
+#
+# Todo:
+#   Add DVB devices (someone with DVB card needs to do this)
+#   Add more analogue devices
+#
+# -----------------------------------------------------------------------
+# Freevo - A Home Theater PC framework
+# Copyright (C) 2002 Krister Lagerstrom, et al.
+# Please see the file freevo/Docs/CREDITS for a complete list of authors.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MER-
+# CHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+#
+# -----------------------------------------------------------------------
+
 #
 import sys, os, glob, re, copy
+from optparse import OptionParser
 try:
     import config
     from config import VideoGroup
@@ -9,6 +47,40 @@ try:
     freevo = True
 except ImportError:
     freevo = False
+
+
+class Options(object):
+    def __init__(self):
+        (self.options, self.args) = self.parse()
+        if freevo and self.options.write:
+            try:
+                self.config_file = open(self.options.config, 'a')
+            except IOError, why:
+                print 'Cannot open %r: %s' % (self.options.config, why)
+                sys.exit(1)
+        else:
+            self.config_file = sys.stdout
+
+    def parse(self):
+        if freevo:
+            self.config_filename = config.overridefile
+        else:
+            self.config_filename = 'stdout'
+
+        parser = OptionParser(conflict_handler='resolve', usage="""
+Scan the video devices to set-up the video groups
+This only works for analogue devices
+
+Usage: %prog [options]""", version='%prog 1.0')
+        parser.add_option('-v', '--verbose', action='count', default=0,
+            help='set the level of verbosity can be set more than once (max 2) [default:%default]')
+        parser.add_option('-c', '--config', action='store', dest='config', default=self.config_filename,
+            help='read the configuration from the file given [default:%default]')
+        parser.add_option('-w', '--write', action='store_true', default=False,
+            help='update config_file [default:%default]')
+        parser.add_option('-d', '--debug', action='store_true', default=False,
+            help='turn on debugging [default:%default]')
+        return parser.parse_args()
 
 
 class AnalyseVideo4Linux(object):
@@ -81,11 +153,14 @@ class AnalyseVideo4Linux(object):
     def _analyse(self):
         """ Try different methods to determine the video families """
         self.devices = os.listdir(self.path)
-        print 'DEBUG: devices:1:', self.devices
+        if options.options.debug:
+            print 'DEBUG: devices:1:', self.devices
         self.devices = self._method1(self.devices)
-        print 'DEBUG: devices:2:', self.devices
+        if options.options.debug:
+            print 'DEBUG: devices:2:', self.devices
         self.devices = self._method2(self.devices)
-        print 'DEBUG: devices:3:', self.devices
+        if options.options.debug:
+            print 'DEBUG: devices:3:', self.devices
         return self.v4l2_devmap
 
     def v4ldevices(self):
@@ -117,12 +192,12 @@ class VideoGroupBuilder(object):
             #print device
             vdev = os.path.join('/dev', device['device'])
             if not os.path.exists(vdev):
-                print '%r does not exist' % vdev
+                print >>sys.stderr, '%r does not exist' % vdev
                 continue
             try:
                 videodev = tv.v4l2.Videodev(vdev)
             except Exception, why:
-                print why
+                print >>sys.stderr, why
                 continue
             #print videodev.__dict__
             group_type = 'unknown'
@@ -139,7 +214,7 @@ class VideoGroupBuilder(object):
             elif videodev.driver in self.NORMAL:
                 group_type = 'normal'
             else:
-                print '%r is an unknown driver' % videodev.driver
+                print >>sys.stderr, '%r is an unknown driver' % videodev.driver
                 continue
 
             vvbi = None
@@ -172,22 +247,23 @@ class VideoGroupBuilder(object):
     def write(self):
         #for vg in self.groups:
         #    print vg.__dict__
-        print 'TV_VIDEO_GROUPS = ['
+        cf = options.config_file
+        print >>cf, 'TV_VIDEO_GROUPS = ['
         for num in range(len(self.groups)):
             vg = self.groups[num]
-            print '  VideoGroup( # %s device, group %s' % (vg.group_type, num)
-            print '    desc=%r,' % vg.desc
-            print '    group_type=%r,' % vg.group_type
-            print '    vdev=%r,' % vg.vdev
-            print '    vvbi=%r,' % vg.vvbi
-            print '    adev=%r,' % vg.adev
-            print '    input_type=%r,' % vg.input_type
-            print '    input_num=%r,' % vg.input_num
-            print '    tuner_norm=%r,' % vg.tuner_norm
-            print '    tuner_chanlist=%r,' % vg.tuner_chanlist
-            print '    record_group=%r' % vg.record_group
-            print '  ),'
-        print ']'
+            print >>cf, '  VideoGroup( # %s device, group %s' % (vg.group_type, num)
+            print >>cf, '    desc=%r,' % vg.desc
+            print >>cf, '    group_type=%r,' % vg.group_type
+            print >>cf, '    vdev=%r,' % vg.vdev
+            print >>cf, '    vvbi=%r,' % vg.vvbi
+            print >>cf, '    adev=%r,' % vg.adev
+            print >>cf, '    input_type=%r,' % vg.input_type
+            print >>cf, '    input_num=%r,' % vg.input_num
+            print >>cf, '    tuner_norm=%r,' % vg.tuner_norm
+            print >>cf, '    tuner_chanlist=%r,' % vg.tuner_chanlist
+            print >>cf, '    record_group=%r' % vg.record_group
+            print >>cf, '  ),'
+        print >>cf, ']'
 
 
     def dump(self):
@@ -210,16 +286,16 @@ class VideoGroupBuilder(object):
                 name = open(os.path.join(video4linux_path, v4ldev, 'name')).read().strip()
                 print '%-8s: %s' % (v4ldev, name)
             print '%s' % ('-' * 41)
-            if freevo:
+            if freevo and options.options.verbose >= 2:
                 v = tv.v4l2.Videodev(os.path.join('/dev', device['device']))
                 v.print_settings()
                 v.close()
-                pass
             print
 
 
 
 if __name__ == '__main__':
+    options = Options()
     # this won't work if procfs is mounted somewhere else
     f = open('/proc/mounts')
     for line in f.readlines():
@@ -227,27 +303,28 @@ if __name__ == '__main__':
         if fields[2] == 'sysfs':
             break
     else:
-        print 'Cannot find mounted sysfs'
+        print >>sys.stderr, 'Cannot find mounted sysfs'
         sys.exit(1)
     sysfs = fields[1]
 
     # Check that there are video4linux devices
     video4linux_path = os.path.join(sysfs, 'class', 'video4linux')
     if not os.path.isdir(video4linux_path):
-        print 'Cannot find video4linux in sysfs'
+        print >>sys.stderr, 'Cannot find video4linux in sysfs'
         sys.exit(1)
 
     analyser = AnalyseVideo4Linux(video4linux_path)
     v4ldevices = analyser.v4ldevices()
 
     if len(analyser.devices) > 0:
-        print 'Devices not checked'
-        print '%s' % ('-' * 41)
+        print >>sys.stderr, 'Devices not checked'
+        print >>sys.stderr, '%s' % ('-' * 41)
         for device in analyser.devices:
-            print device
+            print >>sys.stderr, device
 
     builder = VideoGroupBuilder(v4ldevices)
-    builder.dump()
+    if not freevo or options.options.verbose >= 1:
+        builder.dump()
     if freevo:
         builder.build()
         builder.write()
