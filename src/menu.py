7ba1e8ec6b73c38a715ddffb1589738d67d026d4
Duncan Webb <duncan@freevo.org>
2008-06-02 15:58:57 +0000

    [ 1981293 ] string encoding error Patch from Valera Koval applied
    git-svn-id: svn://svn.freevo.org/freevo/branches/rel-1/freevo@10794 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- src/menu.py
+++ src/menu.py
@@ -69,7 +69,7 @@ class MenuItem(Item):
         if hasattr(self, 'action'):    s += ' action=%s' % self.action
         #if hasattr(self, 'arg') and self.arg: s += ' arg=%s' % self.arg[0]
         if hasattr(self, 'type'):      s += ' type=%s' % self.type
-        if hasattr(self, 'image'):     s += ' image=%s' % self.image
+        if hasattr(self, 'image'):     s += ' image=%s' % String(self.image)
         if hasattr(self, 'icon'):      s += ' icon=%s' % self.icon
         if hasattr(self, 'skin_type'): s += ' skin_type=%s' % self.skin_type
         #if hasattr(self, 'parent'):    s += ' parent=%s' % self.parent
