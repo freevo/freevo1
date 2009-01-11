e5e2d97907927d7a4aaebca10eee59bb93479095
Duncan Webb <duncan@freevo.org>
2009-01-11 18:59:19 +0000

    [ 2497746 ] Alsamixer mute variable error Patch from Gorka Olaizola applied
    git-svn-id: svn://svn.freevo.org/freevo/branches/rel-1/freevo@11239 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- src/plugins/alsamixer.py
+++ src/plugins/alsamixer.py
@@ -218,12 +218,12 @@ class PluginInterface(plugin.DaemonPlugin):
 
     def setMuted(self, mute):
         self.muted = mute
-        if config.MIXER_MAJOR_CTRL_MUTE == 'VOL':
+        if config.MIXER_MAJOR_MUTE_CTRL == 'VOL':
             if mute == 1:
                 self.main_mixer.setmute(1)
             else:
                 self.main_mixer.setmute(0)
-        elif config.MIXER_MAJOR_CTRL_MUTE == 'PCM':
+        elif config.MIXER_MAJOR_MUTE_CTRL == 'PCM':
             if mute == 1:
                 self.pcm_mixer.setmute(1)
             else:
