9d8f64bf33fec146a1eda875469be58455405f4d
Duncan Webb <duncan@freevo.org>
2009-04-26 14:41:31 +0000

    Converted to use optparse Also fixed the processing of multiple remotes in the lircd.conf
    git-svn-id: svn://svn.freevo.org/freevo/branches/rel-1/freevo@11437 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- src/helpers/makelircrc.py
+++ src/helpers/makelircrc.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: iso-8859-1 -*-
 # -----------------------------------------------------------------------
-# makelircrc.py - helper to generate lircrc
+# helper to generate lircrc
 # -----------------------------------------------------------------------
 # $Id$
 #
@@ -32,10 +32,14 @@
 
 
 import re
-import config
 import sys
 import os
+from optparse import IndentedHelpFormatter, OptionParser
+from pprint import pprint
 
+import config
+
+isitem = re.compile('^[ \t]*([^ #\t]*)[ \t]*([\S]*)').match
 iscode = re.compile('^[ \t]*([^ #\t]*)[ \t]*(0x[0-9A-Fa-f]*)').match
 
 # config list to start with
@@ -45,21 +49,58 @@ needed  = []
 mapping = []
 unused  = []
 
+def parse_options():
+    """
+    Parse command line options
+    """
+    import version
+    formatter = IndentedHelpFormatter(indent_increment=2, max_help_position=32, width=100, short_first=0)
+    parser = OptionParser(conflict_handler='resolve', formatter=formatter,
+        usage="freevo %prog [options] [BUTTON=EVENT]",
+        version='%prog ' + str(version._version))
+    parser.prog = os.path.splitext(os.path.basename(sys.argv[0]))[0]
+    parser.description = "Helper to write the freevo lircrc file"
+    parser.add_option('-v', '--verbose', action='count', default=0,
+        help='set the level of verbosity [default:%default]')
+    parser.add_option('--lircrc', metavar='FILE', default=config.LIRCRC,
+        help='the lircrc file [default:%default]')
+    parser.add_option('-r', '--remote', metavar='NAME', default=None,
+        help='the name of the remote in the lircrc file [default:%default]')
+    parser.add_option('-w', '--write', action='store_true', default=False,
+        help='write the lircrc file, this will overwrite an existing file!')
+
+    opts, args = parser.parse_args()
+    return opts, args
+
+
+opts, args = parse_options()
+
 alternatives = {
-    'QUIT'      : 'EXIT',
-    'BACK/EXIT' : 'EXIT',
-    'OSD' : 'DISPLAY',
-    'FF'  : 'FFWD',
-    'FORWARD': 'FFWD',
-    'REV' : 'REW',
-    'REWIND' : 'REW',
-    'PREV'   : 'CH-',
-    'NEXT'   : 'CH+',
-    'MENU/I' : 'MENU',
-    'OK'     : 'SELECT',
-    'GO'     : 'ENTER',
-    'RECORD' : 'REC',
-    }
+    'QUIT'     : 'EXIT',
+    'BACK'     : 'EXIT',
+    'BACK/EXIT': 'EXIT',
+    'OSD'      : 'DISPLAY',
+    'FF'       : 'FFWD',
+    'FORWARD'  : 'FFWD',
+    'REV'      : 'REW',
+    'REWIND'   : 'REW',
+    'PREV'     : 'CH-',
+    'NEXT'     : 'CH+',
+    'MENU/I'   : 'MENU',
+    'OK'       : 'SELECT',
+    'GO'       : 'ENTER',
+    'RECORD'   : 'REC',
+}
+
+for arg in args:
+    try:
+        button, event = arg.split('=')
+        button = button.strip().upper()
+        event  = event.strip().upper()
+        alternatives[button] = event
+        print >>sys.__stderr__, 'Added alternative %r as %r' % (button, event)
+    except ValueError, why:
+        pass
 
 
 def findIncludes(name):
@@ -71,15 +112,72 @@ def findIncludes(name):
             list.append(line.split()[1])
     return list
 
+
+class ParseState:
+    IDLE           = 0
+    IN_REMOTE      = 1
+    IN_CODES       = 2
+    IN_OTHER_CODES = 3
+    def __init__(self):
+        self.state = ParseState.IDLE
+
+    def __str__(self):
+        if self.state == ParseState.IDLE: return 'IDLE'
+        if self.state == ParseState.IN_REMOTE: return 'IN_REMOTE'
+        if self.state == ParseState.IN_CODES: return 'IN_CODES'
+        if self.state == ParseState.IN_OTHER_CODES: return 'IN_OTHER_CODES'
+        return 'UNKNOWN'
+
+
 def parseFile(name):
-    found_codes = 0
+    state = ParseState()
+    num_remotes = 0
+    remotes = []
+    remote_used = None
+    remote_found = False
     file = open(name, 'r')
     for line in file.readlines():
+        if opts.verbose >= 2:
+            print >>sys.__stderr__, '%16s: %s' % (state, line.rstrip())
+        # find the remote section
+        if line.find('begin remote') != -1:
+            remote_found = False
+            num_remotes += 1
+            state.state = ParseState.IN_REMOTE
+        if line.find('end remote') != -1:
+            state.state = ParseState.IDLE
         # find the codes section
         if line.find('begin codes') != -1:
-            found_codes += 1
+            if remote_found:
+                state.state = ParseState.IN_CODES
+            else:
+                state.state = ParseState.IN_OTHER_CODES
+        if line.find('end codes') != -1:
+            state.state = ParseState.IN_REMOTE
+
+        if state.state == ParseState.IN_REMOTE:
+            match = isitem(line)
+            if match:
+                item, value = match.groups()
+                if item.lower() == 'name':
+                    remotes.append(value)
+                    if opts.verbose:
+                        print >>sys.__stderr__, 'remote %s=%s' % (item, value)
+                    if opts.remote:
+                        # if the remote is specified then match its name
+                        if opts.remote.lower() == value.lower():
+                            remote_found = True
+                    else:
+                        # if the remote is *not* specified then use the first remote
+                        if num_remotes == 1:
+                            remote_found = True
+                    if remote_found:
+                        remote_used = value
+                        if opts.verbose:
+                            print >>sys.__stderr__, 'using remote %s' % (value)
+        
         # continue in the codes section
-        if found_codes > 0:
+        if state.state == ParseState.IN_CODES:
             match = iscode(line)
             if match:
                 lirc_button = match.groups()[0]
@@ -94,24 +192,7 @@ def parseFile(name):
                     unused.append(lirc_button)
                     mapping.append((freevo_button, lirc_button))
     file.close()
-    return found_codes
-
-# argument parsing
-if len(sys.argv)>1 and sys.argv[1] in ('-h', '--help'):
-    print 'script to write the freevo lircrc file'
-    print 'usage: makelircrc [-w] [section_index] [button=comand]'
-    print
-    print 'The -w will write the settings to %s' % config.LIRCRC
-    print 'If this is not the file the information should be written to, set'
-    print 'LIRCRC in your local_conf.py to the correct filename. If the file'
-    print 'exists, it will be overwritten.'
-    print
-    print 'If started with no options, this script will print the suggested mapping'
-    print 'and a list of buttons not used right now and a list of events in Freevo'
-    print 'without a button. You can either change EVENTS in local_conf.py or define'
-    print 'a mapping on your own.'
-    print
-    sys.exit(0)
+    return remote_used, remotes
 
 for type in config.EVENTS:
     for button in config.EVENTS[type]:
@@ -121,10 +202,6 @@ for type in config.EVENTS:
 write_option = False
 use_pos = None
 pos = 0
-
-for arg in sys.argv[1:]:
-    if arg == '-w':
-        write_option = True
 
 # first pass: search for included configurations
 for lircdconf in lircdconf_list:
@@ -137,21 +214,26 @@ for lircdconf in lircdconf_list:
 # second pass: read actual key config
 for lircdconf in lircdconf_list:
     if os.path.exists(lircdconf):
-        pos = parseFile(lircdconf)
-        if pos != 1:
-            print "Warning: your %s seems to contain %d sections starting with \"begin codes\"." % (lircdconf, pos)
+        remote_used, remotes = parseFile(lircdconf)
+        if remote_used is None:
+            sys.exit('Failed: Cannot find remote %r, choose from:\n  "' % (opts.remote,) + \
+                '",\n  "'.join(remotes) + '".')
+        if opts.remote is None and len(remotes) != 1:
+            print >>sys.__stderr__, 'Remote not specified using %r from:\n  "' % (remote_used,) + \
+                '",\n  "'.join(remotes) + '".'
 
 # write result to file
-if write_option:
-    out = open(config.LIRCRC, 'w')
+if opts.write:
+    out = open(opts.lircrc, 'w')
     for event, button in mapping:
         out.write('begin\n')
         out.write('    prog   = freevo\n')
         out.write('    button = %s\n' % button)
+        out.write('    repeat = %s\n' % 0)
         out.write('    config = %s\n' % event)
         out.write('end\n')
     out.close()
-    print "Success: %s button mappings written to %s!" % (len(mapping), config.LIRCRC)
+    print >>sys.__stderr__, "Success: %s button mappings written to %r" % (len(mapping), opts.lircrc)
     sys.exit(0)
 
 # print stuff to stdout
@@ -160,31 +242,29 @@ print
 for event, button in mapping:
     print '  %-10s: %-10s' % (button, event)
 print
+
 if unused:
-    print 'The following buttons are not used by Freevo.'
-    print 'You may want to enhance EVENTS in local_conf.py to make use of the buttons'
+    print """The following buttons are not used by Freevo.  You may want to enhance
+EVENTS in local_conf.py to make use of the buttons"""
     print
     for i in unused:
         print '  %s' % i
     print
+
 if needed:
     print 'The following buttons are needed for Freevo and not defined yet.'
-    if unused:
-        print 'Since you have unused buttons, you may want to set a button to a missing one'
-        print 'Right now, the output file will have an unused mapping for %s. You' % unused[0]
-        print 'could assign this button by calling this script with the extra parameter'+\
-              '\'%s=%s\'.' % (unused[0], needed[0])
-        print 'You can add this parameter as often as needed.'
-        print 'Please send good mappings to missing buttons to the Freevo developers to add'
-        print 'them into this script.'
-    else:
-        print 'Since there are no unused buttons anymore, you need to change EVENTS in'
-        print 'local_conf.py'
     print
     for i in needed:
         print '  %s' % i
     print
-#if use_pos == None and pos > 1:
-#    print 'Your %s seems to contain %d sections starting with "begin codes".' % (x.name, pos)
-#    print 'You should select the one best matching your remote by giving its number (1..%d)' % (pos, )
-#    print 'on the commandline.'
+    if unused:
+        print """Since you have unused buttons, you may want to set a button to a missing
+one Right now, the output file will have an unused mapping for %r. You
+could assign this button by calling this script with the extra parameter 
+\'%s=%s\'.  You can add this parameter as often as needed.  Please send
+good mappings to missing buttons to the Freevo developers to add them into
+this script.""" % (unused[0], unused[0], needed[0])
+    else:
+        print """Since there are no unused buttons anymore, you need to change EVENTS in
+local_conf.py"""
+    print
