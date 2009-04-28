fea64cb4ace4ded39f149b5b11cfd0bbc3863fdb
Duncan Webb <duncan@freevo.org>
2009-04-28 14:54:13 +0000

    Updated version to get the version and revision
    git-svn-id: svn://svn.freevo.org/freevo/branches/rel-1/freevo@11444 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- src/version.py
+++ src/version.py
@@ -5,3 +5,13 @@ __version__ = '1.8.4-svn'
 
 runtime  = '0.3.1'
 mmpython = '0.4.10'
+
+try:
+    import revision
+    _revision = revision.__revision__
+except ImportError:
+    _revision = 'Unknown'
+
+_version = __version__
+if _version.endswith('-svn'):
+    _version = _version.split('-svn')[0] + ' r%s' % _revision
