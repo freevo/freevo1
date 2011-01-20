064695f140a78cd302fa489be5be108d53c3efa2
Adam Charrett <adam@dvbstreamer.org>
2011-01-20 22:31:39 +0000

    Fix error in recordings_manager when determine candidates to delete
    git-svn-id: svn://svn.freevo.org/freevo/branches/rel-1/freevo@11799 cc3e1ea1-1e01-0410-8d68-8b121e83a9d5

--- src/tv/plugins/recordings_manager.py
+++ src/tv/plugins/recordings_manager.py
@@ -1073,7 +1073,7 @@ class DiskManager(plugin.DaemonPlugin):
         today = datetime.date.today()
 
         for recorded_item in all_recordings:
-            accessed = time() - os.stat(recorded_item.filename)[7]
+            accessed = time.time() - os.stat(recorded_item.filename)[7]
             if recorded_item.watched and not recorded_item.keep and (accessed > 3600):
                 watched_candidates.append(recorded_item)
 
