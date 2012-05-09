953395a812812fa254be28d6cf35f6205952a7bb
Maciej Urbaniak <maciej@urbaniak.org>
2012-05-09 19:50:49 +0200

    Subtitle plugin baseline

---
+++ src/video/plugins/napiprojekt.py
@@ -0,0 +1,308 @@
+# -*- coding: iso-8859-1 -*-
+# -----------------------------------------------------------------------
+# Plug-in for napiprojekt support
+# -----------------------------------------------------------------------
+# $Id: $
+#
+# Notes: napiprojekt plugin. 
+#        You can donwload subtitles from the http://napiprojekt.pl
+#        with this plugin. Only two langauges supported, Polish and English.
+#        Check out the video.subtitles plugin for more configuration options 
+#
+#        This code is based on some anonymous code found in the depths
+#        of the www :-)
+#
+# Todo:  none
+#
+# BTW:   napi is short in polish for napisy or subtitles in english :-)
+#
+# -----------------------------------------------------------------------
+# Freevo - A Home Theater PC framework
+# Copyright (C) 2002 Krister Lagerstrom, et al.
+# Please see the file freevo/Docs/CREDITS for a complete list of authors.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MER-
+# CHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+#
+# -----------------------------------------------------------------------
+
+__author__           = 'Maciej Mike Urbaniak (maciej@urbaniak.org)'
+__maintainer__       = 'Maciej Mike Urbaniak'
+__maintainer_email__ = 'maciej@urbaniak.org'
+__version__          = 'Revision 0.1'
+__license__          = 'GPL' 
+
+# Module Imports
+import logging
+logger = logging.getLogger('freevo.video.plugins.napiprojekt')
+
+import os
+import urllib
+import tempfile
+import subprocess
+import codecs
+
+import config
+import plugin
+import time
+
+try:
+    from hashlib import md5 as md5
+except ImportError:
+    from md5 import md5
+
+# http://www.joachim-bauch.de/projects/pylzma/
+NAPI_LANGS = [ 'pol', 'eng' ]
+
+from subtitles import SubsHandler, SubsError, Subtitles
+
+class PluginInterface(plugin.Plugin):
+    """
+    This is a handler plugin for http://napiprojekt.pl subtitle provider
+    and is used by main subtitle plugin.
+    
+    Only polish and english subtitles are supported by napiprojekt.pl
+
+    Activate with:
+    | plugin.activate('video.napiprojekt')
+    
+    and make sure the SUBS_HANDLERS = [ ('video.napiprojekt'), ]
+    is set for the main subtitles plugin to be able to use this plugin
+    
+    and of course make sure the main subtitles plugin is activated too:
+    | plugin.activate('video.subtitles')
+
+    """
+
+    def __init__(self):
+        if not config.SYS_USE_NETWORK:
+            self.reason = 'SYS_USE_NETWORK not enabled'
+            return
+        
+        if not plugin.is_active('video.subtitles'):
+            self.reason = 'Plugin \'video.subtitles\' not active, activate it first in your local_config.py!'
+            return
+
+        self.handler = NapiHandler()
+        
+        plugin.Plugin.__init__(self)
+
+        plugin.register(self, 'video.napiprojekt')
+
+
+    def config(self):
+        """returns the config variables used by this plugin"""
+        return [
+            ('NAPI_LANGS', [ 'pol', 'eng', ], 
+                'All Supported by napiprojekt.pl ISO 639-2 subtitles language codes'),
+            ('NAPI_LANG_MAP', { 'pol': ('PL'), 'eng': ('ENG') },
+                'Maps ISO 639-2 lang code to the one used by napiprojekt.pl'), 
+            ('NAPI_PWD', 'iBlm8NTigvru0Jr0',
+                'Password for the compressed file'), 
+            ('NAPI_URL', 'http://napiprojekt.pl/unit_napisy/dl.php?l=%s&f=%s&t=%s&v=other&kolejka=false&nick=&pass=&napios=%s',
+                'Napiprojekt fetch URL'),
+        ]
+
+
+    def __getitem__(self, key):
+        """
+        Get the item's attribute.
+
+        @returns: the specific attribute
+        """
+        if key == 'name': 
+            return self.handler.name
+            
+        if key == 'id': 
+            return self.handler.id
+
+        if key == 'handler': 
+            return self.handler
+
+        return plugin.Plugin.__getitem__(self, key)        
+
+
+class NapiSubtitles(Subtitles):
+    def __init__(self, handler, vfile, lang, hash):
+        self.hash = hash
+        Subtitles.__init__(self, handler, vfile, lang)
+
+
+    def download(self):
+        logger.info('Downloading subtitles for %s in %s', self.vfile, self.lang)
+
+        # TODO try to hash again, if not hashed already
+        if not self.hash:
+            return False
+            
+        data = None
+
+        url = config.NAPI_URL % \
+            (config.NAPI_LANG_MAP[self.lang], self.hash.hexdigest(), self._f(self.hash.hexdigest()), os.name)
+
+        http_code = 200
+        try:
+            data = urllib.urlopen(url)
+            if hasattr(data, 'getcode'):
+                http_code = data.getcode() 
+            data = data.read()
+        except (IOError, OSError), e:
+            logger.warning('Fetching subtitles failed: %s', e)
+            return False
+
+        if http_code != 200:
+            logger.warning('Fetching subtitles failed, HTTP code: %s', str(http_code))
+            return False
+
+        if data.startswith('NPc'):
+            logger.warning('Subtitles for %s in %s not found', self.vfile, self.lang)
+            return False
+            
+        self.data = data
+        self.compressed = True
+       
+        logger.info('Downloaded subtitles for %s in %s', self.vfile, self.lang)
+
+        return True
+
+
+    def save(self):
+        """
+        Saves downloaded subtitles
+        """
+        logger.info('Saving subtitles for %s in %s', self.vfile, self.lang)
+       
+        if self.compressed:
+            self._decompress()
+            
+        # we need to back up old subs if exist before we actually overwrite the old subs
+        self.backup()
+
+        logger.info('Writing file %s', self.sfile)
+
+        fp = codecs.open(self.sfile,'wb')
+        fp.write(self.data)
+        fp.close()
+
+        logger.info('Subtitle file %s written (%d bytes)', self.sfile, len(self.data))
+        
+        return True
+
+
+    """
+    Class' private methods below
+    """
+
+    def _decompress(self):
+        fp = tempfile.NamedTemporaryFile('wb', suffix=".7z")
+        tfp = fp.name
+        fp.write(self.data)
+        fp.flush()
+
+        try:
+            cmd = ['/usr/bin/7z', 'x', '-y', '-so', '-p' + config.NAPI_PWD, tfp]
+            sa = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
+            (so, se) = sa.communicate(self.data)
+            self.data = so
+            retcode = sa.returncode
+            logger.debug('Executing cmd %s', ' '.join(cmd))
+
+        except OSError, e:
+            fp.close()
+            msg = 'Skipping, subtitle decompression failed: %s' % (e)
+            logger.warning(msg)
+            raise SubsError(msg)
+
+        fp.close()
+
+        logger.debug("Decompressed subtitles for %s" % (self.lang))
+
+
+    def _f(self, z):
+        idx = [ 0xe, 0x3,  0x6, 0x8, 0x2 ]
+        mul = [   2,   2,    5,   4,   3 ]
+        add = [   0, 0xd, 0x10, 0xb, 0x5 ]
+
+        b = []
+        for i in xrange(len(idx)):
+            a = add[i]
+            m = mul[i]
+            i = idx[i]
+
+            t = a + int(z[i], 16)
+            v = int(z[t:t+2], 16)
+            b.append( ("%x" % (v*m))[-1] )
+
+        return ''.join(b)
+
+
+class NapiHandler(SubsHandler):
+
+    def __init__(self):
+        langs = []
+        
+        try:
+            langs = config.NAPI_LANGS
+        except:
+            langs = NAPI_LANGS
+            
+        SubsHandler.__init__(self, 'np', 'napiprojekt.pl', langs)
+
+
+    def __getitem__(self, key):
+        """
+        Get the item's attribute.
+
+        @returns: the specific attribute
+        """
+        return SubsHandler.__getitem__(self, key)
+
+
+    def get_subs(self, vfile, langs_):
+        # based on requested languages, create intersect of capabilites vs. request
+        langs = filter(lambda x: x in langs_, self.langs)
+        subs  = {}
+        hash  = self._hash(vfile)
+
+        if not hash:
+            logger.warning('Hashing of %s failed, aborting...', vfile)
+            return subs
+
+        for lang in langs:
+            sub = NapiSubtitles(self, vfile, lang, hash)
+            if not sub.download():
+                continue
+                
+            subs[sub['id']] = sub
+
+        return subs
+        
+        #return [map(lambda x: x['lang'], self.subs)]
+        
+
+    """
+    Class' private methods below
+    """
+    def _hash(self, vfile):
+        hash  = md5()
+
+        try:
+            hash.update(open(vfile).read(10485760))
+        except (IOError, OSError), e:
+            logger.warning('Hashing failed: %s', e)
+            return None
+
+        return hash    
+
+
---
+++ src/video/plugins/subtitles.py
@@ -0,0 +1,542 @@
+# -*- coding: iso-8859-1 -*-
+# -----------------------------------------------------------------------
+# Plug-in for subtitles download and support/maintenance
+# -----------------------------------------------------------------------
+# $Id: $
+#
+# Notes: subtitles plugin. 
+#        You can donwload subtitles from the http://napiprojekt.pl
+#        with this plugin. Only two langgauges supported, polish and english.
+#        Add following to your local_conf.py
+#        SUBS_LANGS = { 'pol': ('Polish'), 'eng': ('English') }
+#        if you want both, polish and english subtitles, or just use the lang
+#        of your choice, either polish or english.
+#        SUBS_AUTOACCEPT = True
+#        if you want to blindly download all subs available, otherwise the 
+#        menu with available aubtitles will be presented and choice can be made
+#        SUBS_FORCE_UPDATE = True
+#        if you want to override existing subtitle file (if exists)
+#        SUBS_FORCE_BACKUP = True
+#        and backup old file if in update mode above
+#        activate with plugin.activate('video.napi')
+#        You can also set subs_search on a key (e.g. 't') by setting
+#        EVENTS['menu']['t'] = Event(MENU_CALL_ITEM_ACTION, arg='subs_search')
+#
+# Todo:  none
+#
+# BTW:   napi is short in polish for napisy or subtitles in english :-)
+#
+# -----------------------------------------------------------------------
+# Freevo - A Home Theater PC framework
+# Copyright (C) 2002 Krister Lagerstrom, et al.
+# Please see the file freevo/Docs/CREDITS for a complete list of authors.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MER-
+# CHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+#
+# -----------------------------------------------------------------------
+
+__author__           = 'Maciej Mike Urbaniak (maciej@urbaniak.org)'
+__maintainer__       = 'Maciej Mike Urbaniak'
+__maintainer_email__ = 'maciej@urbaniak.org'
+__version__          = 'Revision 0.1'
+__license__          = 'GPL' 
+
+# Module Imports
+import logging
+logger = logging.getLogger('freevo.video.plugins.subtitles')
+
+import os
+import glob
+from operator import itemgetter, attrgetter
+
+import menu
+import config
+import plugin
+import re
+import time
+import util
+
+from gui.PopupBox import PopupBox
+
+HEAD = 'head'
+TAIL = 'tail'
+
+def trunc(what, cnt, where=HEAD, ellipsis='...'):
+    if where == HEAD:
+        return (ellipsis + what[len(what) - (cnt - len(ellipsis)):]) if len(what) > cnt else what
+    return (what[:len(what) - (cnt + len(ellipsis))] + ellipsis) if len(what) > cnt else what
+
+
+class Error(Exception):
+    """Base class for exceptions in Subs"""
+    def __str__(self):
+        return self.message
+    def __init__(self, message):
+        self.message = message
+
+
+class SubsError(Error):
+    """used to raise Subtitle specific exceptions"""
+    pass
+
+
+class SubsHandler():
+    """
+    Base Subtitles handler class definition including all methods that subclasses 
+    shall overwrite
+    """
+    
+    def __init__(self, id, name, langs):
+        self.id    = id
+        self.name  = name
+        self.langs = langs
+
+    
+    def __getitem__(self, key):
+        """
+        Get the item's attribute.
+        @returns: the specific attribute
+        """
+        if key == 'name':
+            return self.name
+
+        if key == 'id': 
+            return self.id
+        
+        if key == 'langs': 
+            return self.langs
+            
+        return ''
+
+    
+    def get_subs(self, vfile_, langs_):
+        """
+        Derived class must overwrite this method
+
+        Get all available subtitles for the item
+        @returns: the collection of subtitles keyed by the subtitle id
+        """
+        pass
+        
+
+class Subtitles():
+
+    def __init__(self, handler, vfile, lang):
+        self.id         = 0
+        self.handler    = handler
+        self.lang       = lang
+        self.vfile      = vfile
+        self.vbase      = os.path.splitext(self.vfile)[0]
+        self.sfile      = ''
+        self.compressed = False
+        self.data       = None
+        self.fmt        = 'txt'
+
+
+    def __getitem__(self, key):
+        """
+        Get the item's attribute.
+
+        @returns: the specific attribute
+        """
+        if key == 'id': 
+            return hash((self.handler['id'], self.lang, self.vfile))
+            
+        if key == 'handler': 
+            return self.handler
+        
+        if key == 'vfile': 
+            return self.vfile
+
+        if key == 'sfile': 
+            return self.sfile
+
+        if key == 'lang': 
+            return self.lang
+
+        if key == 'data': 
+            return self.data
+
+
+    def download(self):
+        """
+        Derived class must overwrite this method
+
+        Physically download the subtitle files
+        @returns: True or False
+        """
+        pass
+
+
+    def save(self):
+        """
+        Derived class must overwrite this method
+
+        Physically write subtitle file to the local storage
+        """
+        pass
+
+
+    def backup(self):
+        """
+        Backs up existing subtitle files
+        """
+        if config.SUBS_FORCE_LANG_EXT == True:
+            self.sfile = '%s.%s.%s.%s' % (self.vbase, self.handler['id'], self.lang, self.fmt)
+        else:
+            self.sfile = '%s.%s.%s' % (self.vbase, self.handler['id'], self.fmt)
+
+        if not config.SUBS_FORCE_UPDATE and os.path.exists(self.sfile):
+            msg = 'Skipping, subtitles %s aready exist and forced update is disabled' % (self.sfile)
+            logger.warning(msg)
+            raise SubsError(msg)
+
+        if config.SUBS_FORCE_BACKUP and os.path.exists(self.sfile):
+            vfile_bkp = self.sfile + '.bkp'
+            try:
+                if os.path.exists(vfile_bkp):
+                    os.remove(vfile_bkp)
+
+                os.rename(self.sfile, vfile_bkp)
+
+            except (IOError, OSError), e:
+                msg = 'Skipping due to backup of \'%s\' as \'%s\' failure: %s' % (self.sfile, vfile_bkp, e)
+                logger.warning(msg)
+                raise SubsError(msg)
+            else:
+                logger.info('Old subtitle backed up as \'%s\'', vfile_bkp)
+
+        return True
+
+    
+class PluginInterface(plugin.ItemPlugin):
+    """
+    You can add subtitles for video items with the plugin.
+
+    Activate with:
+    | plugin.activate('video.subtitles')
+    
+    Make sure the suitable subtitles handler plugin is activated too:
+    | plugin.activate('video.napiprojekt')
+    
+    and/or
+    | plugin.activate('video.opensubtitles')
+    
+    etc.
+
+    Make sure the 
+    | SUBS_HANDLERS = [ ('video.napiprojekt'), ('video.opensubtitles') ]
+    is set in your local_config.py for the this subtitles plugin 
+    to be able to use the available handlers
+
+    You can also set subs_search on a key (e.g. 't') by setting
+    | EVENTS['menu']['t'] = Event(MENU_CALL_ITEM_ACTION, arg='subs_search')
+    """
+
+    def __init__(self):
+        if not config.SYS_USE_NETWORK:
+            self.reason = 'SYS_USE_NETWORK not enabled'
+            return
+        
+        self.handlers = {}
+        self.subs     = {}
+        self.subfiles = []
+
+        try:
+            handlers = config.SUBS_HANDLERS
+        except:
+            self.reason = 'Plugin \'video.subtitles\' activated but SUBS_HANDLERS not defined!'
+            return
+
+        logger.info('Available Handlers : %s', handlers)
+
+        for item in handlers:
+            if not plugin.is_active(item):
+                logger.warning('Plugin \'%s\' listed as available but not activated, activating now!', item)
+                plugin.activate(item)
+
+            handler = plugin.getbyname(item)['handler']
+            self.handlers[handler['id']] = handler
+            logger.info('Successfuly loaded subtitle handler %s', handler.name)
+
+        plugin.ItemPlugin.__init__(self)
+
+
+    def config(self):
+        """
+        Returns the config variables used by this plugin
+        """
+        return [
+            ('SUBS_LANGS',          { 'pol': ('Polish'), 'eng': ('English'), 'ger': ('German') },
+                'Subtitles to download'),
+            ('SUBS_EXTS',           [ '.srt', '.sub', '.txt', '.ssa', '.smi', '.ass', '.mpl'], 
+                'Known subtitles file extensions'),
+            ('SUBS_AUTOACCEPT',     False, 
+                'Autoaccept all found subtitles, language coded filenames'),
+            ('SUBS_FORCE_UPDATE',   True,  
+                'Force update of existing subtitles'),
+            ('SUBS_FORCE_BACKUP',   True,  
+                'Force backup of existing subtitles'),
+            ('SUBS_FORCE_LANG_EXT', True,
+                'Force use of the subtitle lagnguage code in the filename i.e. xxx.eng.txt'),
+        ]
+
+
+    def actions(self, item):
+        """
+        Actions this item supports
+        """
+        self.item = item
+        subs      = False
+
+        if hasattr(self.item, 'subitems') and self.item.subitems:
+            for i in range(len(self.item.subitems)):
+                subs = self.check_existing_subs(self.item.subitems[i].filename)
+        else:
+            subs = self.check_existing_subs(self.item.filename)
+
+        if item.type == 'video' and item.mode == 'file':
+            if subs and not config.SUBS_FORCE_BACKUP:
+                return [ ( self.subs_delete , _('Delete Subtitles'),   'subs_delete') ]
+            elif subs and config.SUBS_FORCE_BACKUP:
+                return [ ( self.subs_delete , _('Delete Subtitles'),   'subs_delete'),
+                         ( self.subs_search , _('Download Subtitles'), 'subs_search') ]
+            else:
+                return [ ( self.subs_search , _('Download Subtitles'), 'subs_search') ]
+      
+        return []
+
+    
+    def check_existing_subs(self, file):
+        """
+        Check if any existing subitle files that match the pattern 
+        """
+        return len(self.get_existing_subs(file))
+
+
+
+    def get_existing_subs(self, file):
+        """
+        Get all existing subitle files that match the pattern 
+        into the collection and return it.
+        """
+        # if there are literal chars like '[' or ']' in the filename then glob will 
+        # not match filenames. Need to replace them with '?'. Most likely it confuses
+        # regex inside glob
+        base = re.sub('[\]\[]+', '?', os.path.splitext(file)[0])
+
+        return [n for n in glob.glob(base + '*') \
+            if os.path.splitext(n)[1] in config.SUBS_EXTS]
+
+
+
+    def subs_search(self, arg=None, menuw=None):
+        """
+        Search subtitle website for subtitles for this item
+        """
+        self.subs = {}
+        items     = []
+    
+        try:
+            #get the subtitles from each active handler
+            for handler in self.handlers.values():
+                box = PopupBox(text=_('Searching %s for subtitles...' % (handler['name'])))
+                box.show()
+
+                if self.item.subitems:
+                    for i in range(len(self.item.subitems)):
+                        self.subs.update(handler.get_subs(self.item.subitems[i].filename, config.SUBS_LANGS.keys()))
+                else:
+                    self.subs.update(handler.get_subs(self.item.filename, config.SUBS_LANGS.keys()))
+
+                box.destroy()
+            
+            for subs in sorted(self.subs.values(), key=attrgetter('handler.id', 'lang', 'vfile')):
+                try:
+                    lang = config.SUBS_LANGS[subs['lang']]
+                    if self.item.subitems:
+                        items.append(menu.MenuItem(_('%s subtitles for "%s" (from %s)' % \
+                                     (lang, trunc(os.path.basename(subs['vfile']), 20), subs.handler['name'])),
+                                     self.subs_create_subs, (subs['id'])))
+                    else:
+                        items.append(menu.MenuItem(_('%s subtitles (from %s)' % \
+                                     (lang, subs.handler['name'])),
+                                     self.subs_create_subs, (subs['id'])))
+                    
+                except Unicode, e:
+                    print e
+
+            # if we have more then 1 set of subs, we give user an option to save all
+            if len(self.subs) > 1:
+                items.insert(0, menu.MenuItem(_('Get all available subtitles listed below'),
+                             self.subs_create_subs, ('all')))
+
+        except (Exception), error:
+            logger.error('%s' % (error,))
+            box.destroy()
+            box = PopupBox(text=_('Connection to service failed: ') + str(error))
+            box.show()
+            time.sleep(2)
+            box.destroy()
+            return
+
+        if config.SUBS_AUTOACCEPT and len(items > 0):
+            self.subs_create_subs(arg=('all'), menuw=menuw)
+            return
+
+        if items:
+            moviemenu = menu.Menu(_('Subtitles Query'), items)
+            menuw.pushmenu(moviemenu)
+            return
+
+        box = PopupBox(text=_('No subtitles available'))
+        box.show()
+        time.sleep(2)
+        box.destroy()
+        return
+
+
+    def subs_delete(self, arg=None, menuw=None):
+        """
+        Delete subtitles for this item
+        """
+        items         = []
+        self.subfiles = []
+
+        # loop through the results and create menu
+        # first we need to see if there are existing subs that need to be maintained 
+        # (potentially deleted so to speak)
+        if hasattr(self.item, 'subitems') and self.item.subitems:
+            for i in range(len(self.item.subitems)):
+                self.subfiles.extend(self.get_existing_subs(self.item.subitems[i].filename))
+        else:
+            self.subfiles = self.get_existing_subs(self.item.filename)
+
+        for subfile in self.subfiles:
+            try:
+                items.append(menu.MenuItem(_('%s' % (os.path.split(subfile)[1])),
+                             self.subs_delete_subs, (subfile)))
+            except Unicode, e:
+                print e
+
+        # if we have more then 1 set of subs, we give user an option to save all
+        if len(self.subfiles) > 1:
+            items.insert(0, menu.MenuItem(_('Delete all subtitle files listed below'),
+                         self.subs_delete_subs, ('all')))
+
+        if config.SUBS_AUTOACCEPT and len(items > 0):
+            self.subs_delete_subs(arg='all', menuw=menuw)
+            return
+
+        if items:
+            moviemenu = menu.Menu(_('Subtitles Query'), items)
+            menuw.pushmenu(moviemenu)
+            return
+
+        box = PopupBox(text=_('No subtitles to be deleted for %s' % (os.path.base(self.item.filename))))
+        box.show()
+        time.sleep(2)
+        box.destroy()
+        return
+
+
+    def subs_menu_back(self, menuw):
+        """
+        check how many menus we have to go back to see the item
+        """
+        import directory
+
+        # check if we have to go one menu back (called directly) or
+        # two (called from the item menu)
+        back = 1
+        if menuw.menustack[-2].selected != self.item:
+            back = 2
+
+        # maybe we called the function directly because there was only one
+        # entry and we called it with an event
+        if menuw.menustack[-1].selected == self.item:
+            back = 0
+
+        # update the directory
+        if directory.dirwatcher:
+            directory.dirwatcher.scan()
+
+        # go back in menustack
+        for i in range(back):
+            menuw.delete_menu()
+        menuw.refresh()
+
+
+    def subs_create_subs(self, arg=None, menuw=None):
+        """
+        create subs for the item
+        """
+        box = PopupBox(text=_('Saving subtitles...'))
+        box.show()
+
+        try:
+            if arg == None or arg == 'all':
+                # we write all available subs
+                for subs in self.subs.values():
+                    subs.save()
+            else:
+                # we write only chosen subs
+                subs = self.subs[arg]
+                logger.debug('Writing subs from %s for lang %s', subs.handler['name'], subs['lang'])
+                subs.save()
+                        
+        except (Exception), error:
+            logger.error('%s' % (error,))
+            box.destroy()
+            box = PopupBox(text=_(error))
+            box.show()
+            time.sleep(2)
+            
+        # reset subs
+        self.subs = {}
+
+        self.subs_menu_back(menuw)
+        box.destroy()
+ 
+
+    def subs_delete_subs(self, arg=None, menuw=None):
+        """
+        delete subtitle file(s) for the item
+        """
+        box = PopupBox(text=_('Deleting subtitles...'))
+        box.show()
+
+        try:
+            if arg == None or arg == 'all':
+                # we delete all available subtitle files
+                for subs in self.subfiles:
+                    os.remove(subs)
+            else:
+                # we delete only chosen subtitle file
+                logger.debug('Deleting subtitle file %s', arg)
+                os.remove(arg)
+
+        except (Exception), error:
+            logger.error('%s' % (error,))
+            box.destroy()
+            box = PopupBox(text=_(error))
+            box.show()
+            time.sleep(2)
+            
+        self.subfiles = []
+
+        self.subs_menu_back(menuw)
+        box.destroy()
+ 
